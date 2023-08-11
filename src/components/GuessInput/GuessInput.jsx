0cafaee55c164490644328fea9dff19ab940b9d4
Sam <aquatomics@gmail.com>
2023-08-11 10:57:01 -0600

    Add checkIfCorrect props

--- src/components/GuessInput/GuessInput.jsx
+++ src/components/GuessInput/GuessInput.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function GuessInput({ guesses, setGuesses }) {
+function GuessInput({ guesses, setGuesses, checkIfCorrect, gameStatus }) {
   const [guess, setGuess] = React.useState("")
 
   return (
@@ -8,6 +8,7 @@ function GuessInput({ guesses, setGuesses }) {
       className="guess-input-wrapper"
       onSubmit={(e) => {
         e.preventDefault()
+        checkIfCorrect(guess)
         const guessWords = [...guesses, guess]
 
         setGuesses(guessWords)
@@ -17,6 +18,8 @@ function GuessInput({ guesses, setGuesses }) {
     >
       <label htmlFor="guess-input">Enter guess:</label>
       <input 
+        required
+        disabled={gameStatus !== "running"}
         id="guess-input"
         type="text"
         pattern=".{5,5}"
