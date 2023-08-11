5b9f68508130b7d84328a569074a1e577bbc07f0
Sam <aquatomics@gmail.com>
2023-08-11 10:56:28 -0600

    Add checkIfCorrect and ganeStatus state

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput';
 import RenderingGuess from '../RenderingGuess/RenderingGuess';
+import FinalMessage from '../FinalMessage/FinalMessage';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -12,14 +13,29 @@ console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([])
+  const [gameStatus, setGameStatus] = React.useState("running")
+
+  function checkIfCorrect(guess) {
+    if (guess === answer) {
+      setGameStatus("win")
+    } else if (guesses.length >= NUM_OF_GUESSES_ALLOWED) {
+      setGameStatus("lost")
+    }
+  }
 
   return <>
     <RenderingGuess 
       guesses={guesses}
+      answer={answer}
+    />
+    <FinalMessage 
+      gameStatus={gameStatus}
     />
     <GuessInput 
       guesses={guesses}
       setGuesses={setGuesses}
+      checkIfCorrect={checkIfCorrect}
+      gameStatus={gameStatus}
     />
   </>
 }
